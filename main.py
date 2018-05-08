81bb5a405b30b85003e28244782a1429e347ce54
Giuseppe <giuseppe.parasilitipalumbo@studium.unict.it>
2018-05-08 11:40:04 +0200

    Aggiornamento 08/05/18
    1. Generazione tabella output Mithril automaticamente

--- main.py
+++ main.py
@@ -88,21 +88,40 @@ def process3():
     outMainFile = pathtempAnalysis + request.form['outMainFile']
 
     rootDir = os.getcwd() + os.sep
+    flag = False
+    html = "<thead><tr>"
 
     if inputFileName:
         if os.path.exists(pathtempAnalysis + inputFileName):
             try:
-                sp.call(
-                    ['java', '-jar', rootDir + 'library/java/MITHrIL2.jar', 'merged-mithril', '-verbose', '-i', pathtempAnalysis+inputFileName, '-o', outMainFile, '-p',
-                     outPertubationsName])#, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
-                return jsonify({'status': 1, 'type': 'success', 'mess': 'I file output di <b>MITHrIL</b>, sono stati generati con successo!'})
+                sp.call(['java', '-jar', rootDir + 'library/java/MITHrIL2.jar', 'merged-mithril', '-verbose', '-i',
+                         pathtempAnalysis + inputFileName, '-o', outMainFile, '-p',
+                         outPertubationsName])  # , stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+                with open(rootDir + pathtempAnalysis + inputFileName, "r") as myfile:
+                    for line in myfile:
+                        temp = line.split("\t")
+                        # creo la stringa per le colonne del dataset
+                        if (flag == False):
+                            for elem in temp:
+                                html += "<th>" + elem.rstrip() + "</th>"
+                                html += "</tr></thead><tbody>"
+                            flag = True
+                        else:
+                            html += "<tr>"
+                            for elem in temp:
+                                html += "<td>" + elem.rstrip() + "</td>"
+                                html += "</tr>"
+                                html += "</tbody>"
+                    return jsonify({'status': 1, 'type': 'success', 'tableHtml': html,
+                                    'mess': 'I file output di <b>MITHrIL</b>, sono stati generati con successo!'})
             except:
-                return jsonify({'status': 0, 'type': 'error', 'mess': 'Si è verificato un problema durante l\'esecuzione del jar'})
+                return jsonify(
+                    {'status': 0, 'type': 'error', 'mess': 'Si è verificato un problema durante l\'esecuzione del jar'})
         else:
-            return jsonify({'status': 0, 'type': 'error', 'mess': 'Il file di input non è stato trovato, potrebbe non essere stato generato!'})
+            return jsonify({'status': 0, 'type': 'error',
+                            'mess': 'Il file di input non è stato trovato, potrebbe non essere stato generato!'})
     else:
         return jsonify({'status': 0, 'type': 'error', 'mess': 'Si è verificato un problema durante l\'esecuzione!'})
-
 
 
 if __name__ == '__main__':
